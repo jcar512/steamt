bd452c2d777b34d9af0fd3368b0c5fbd6b4927b0
jcar512 <110567627+jcar512@users.noreply.github.com>
2024-06-21 20:10:31 -0300

    working on crud

--- js/crud.js
+++ js/crud.js
@@ -1,8 +1,42 @@
 const crudForm = document.querySelector("#crudForm");
 
+function addNewGame() {
+  const gameList = JSON.parse(localStorage.getItem("gameList"));
+
+  let gameId = 1;
+
+  let idIsAlreadyUsed = gameList.find((game) => game.id === gameId);
+
+  while (idIsAlreadyUsed) {
+    gameId++;
+    idIsAlreadyUsed = gameList.find((game) => game.id === gameId);
+  }
+
+  const formData = new FormData(crudForm);
+
+  const area = formData.get("categories");
+  const lines = area
+    .replace(/\r\n/g, "\n")
+    .split("\n")
+    .filter((line) => line); //https://stackoverflow.com/a/59897290
+
+  console.log(lines);
+  const game = {
+    id: gameId,
+    title: formData.get("title"),
+    description: formData.get("description"),
+    price: formData.get("price"),
+    categories: lines,
+    img: formData.get("gameImg"),
+  };
+
+  gameList.push(game);
+  console.log(gameList);
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   crudForm.addEventListener("submit", function (event) {
     event.preventDefault();
-    console.log(event);
+    addNewGame();
   });
 });
