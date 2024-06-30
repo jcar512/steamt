980fc668a522696227a4846677a4c1fbb3ca1fa2
jcar512 <110567627+jcar512@users.noreply.github.com>
2024-06-30 00:09:36 -0300

    final touchs

--- utils/handleStatistics.js
+++ utils/handleStatistics.js
@@ -4,10 +4,47 @@ export function listSales() {
   const soldGames = JSON.parse(localStorage.getItem("sales"));
 
   if (soldGames) {
-    soldGames.forEach((sale) => {
+    const li = document.createElement("li");
+    salesList.appendChild(li);
+    li.classList = "grid grid-cols-3 space-x-3 border-b";
+
+    const p = document.createElement("p");
+    li.appendChild(p);
+    p.classList = "border-r text-center font-bold";
+    p.innerText = "Título";
+
+    const p2 = document.createElement("p");
+    li.appendChild(p2);
+    p2.classList = "border-r text-center font-bold";
+    p2.innerText = "Ventas";
+
+    const p3 = document.createElement("p");
+    li.appendChild(p3);
+    p3.classList = "text-center font-bold";
+    p3.innerText = "Recaudado";
+
+    soldGames.forEach((sale, index) => {
       const li = document.createElement("li");
       salesList.appendChild(li);
-      li.innerText = `Juego: ${sale.title} | Ventas: ${sale.quantity} | Total recaudado: ${sale.revenue} `;
+      li.classList =
+        index === soldGames.length - 1
+          ? "grid grid-cols-3 space-x-3"
+          : "grid grid-cols-3 space-x-3 border-b";
+
+      const p = document.createElement("p");
+      li.appendChild(p);
+      p.classList = "border-r text-center";
+      p.innerText = sale.title;
+
+      const p2 = document.createElement("p");
+      li.appendChild(p2);
+      p2.classList = "border-r text-center";
+      p2.innerText = sale.quantity;
+
+      const p3 = document.createElement("p");
+      li.appendChild(p3);
+      p3.classList = "text-center";
+      p3.innerText = `UYU ${sale.revenue}`;
     });
   }
 }
